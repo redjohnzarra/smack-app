22e6acb0900ca57689ba9641fc50b6c5cf123544
Reden John Zarra <redjohnzarra@gmail.com>
2019-02-07 17:40:02 +0800

    Login ui

---
+++ smack-app/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  smack-app
+//
+//  Created by Reden John Zarra on 07/02/2019.
+//  Copyright © 2019 Reden John Zarra. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ smack-app/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack-app
+//
+//  Created by Reden John Zarra on 07/02/2019.
+//  Copyright © 2019 Reden John Zarra. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "to_login"
