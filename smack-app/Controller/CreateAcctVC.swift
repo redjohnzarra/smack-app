39333996dc09668133c141354db46d995e437f60
Reden John Zarra <redjohnzarra@gmail.com>
2019-02-08 15:29:07 +0800

    Changes

--- smack-app/Controller/CreateAcctVC.swift
+++ smack-app/Controller/CreateAcctVC.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class CreateAcctVC: UIViewController {
-
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND_TO_CHANNEL, sender: nil)
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        // Where or if ang , . guard let unwrap optional values
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passTxt.text , passTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("User registered successfully!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
     }
 }
