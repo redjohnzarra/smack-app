7c4a4ef7bb3a5750b5671d20a3f9d2077cf79be7
Reden John Zarra <redjohnzarra@gmail.com>
2019-02-08 15:18:47 +0800

    Added register api conn

---
+++ smack-app/Services/AuthService.swift
@@ -0,0 +1,72 @@
+//
+//  AuthService.swift
+//  smack-app
+//
+//  Created by Reden John Zarra on 08/02/2019.
+//  Copyright © 2019 Reden John Zarra. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    // CompletionHandler is from constants
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result.error == nil {
+                completion(true) // Custom completion handler
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any) // Print error
+            }
+        }
+        
+    }
+    
+}
--- smack-app/Utilities/Constants.swift
+++ smack-app/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> () // typealias is like assigning a name to Type (Custom Type Name)
+
 // Segues
 let TO_LOGIN = "to_login"
 let TO_CREATE_ACCT = "to_create_account"
 let UNWIND_TO_CHANNEL = "unwind_to_channel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// URL Constants
+let BASE_URL = "https://smackchaton.herokuapp.com/v1"
+let URL_REGISTER = "\(BASE_URL)/account/register"
